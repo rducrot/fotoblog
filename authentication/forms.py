3d2a135b5e9bccd937c0164edea57d46ccaf2eaf
rducrot <ducrot.romain@gmail.com>
2022-11-16 15:11:55 +0100

    Gestion ajout photo de profil

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,8 +1,15 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from django.forms import ModelForm
 
 
 class SignupForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
         fields = ('username', 'email', 'first_name', 'last_name', 'role')
+
+
+class UploadProfilePhotoForm(ModelForm):
+    class Meta:
+        model = get_user_model()
+        fields = ('profile_photo',)
--- authentication/views.py
+++ authentication/views.py
@@ -5,6 +5,17 @@ from django.shortcuts import redirect, render
 from . import forms
 
 
+def upload_profile_photo(request):
+    form = forms.UploadProfilePhotoForm(instance=request.user)
+    if request.method == "POST":
+        form = forms.UploadProfilePhotoForm(request.POST, request.FILES, instance=request.user)
+        if form.is_valid():
+            request.user.profile_photo = form.save()
+            return redirect("home")
+    return render(request, "authentication/upload_profile_photo.html",
+                  context={"form": form})
+
+
 def signup_page(request):
     form = forms.SignupForm()
     if request.method == 'POST':
